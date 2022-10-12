76613f9202a1448794fbdc408b6a6e9bcd012250
Caleb Cox <canac@users.noreply.github.com>
2022-10-12 18:37:14 -0500

    Remove unused COOKIE_SECRET environment variable

--- app/lib/env.ts
+++ app/lib/env.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
+import { nonEmptyString } from "./zodTypes";
 
 const envSchema = z.object({
-  PORT: z.string().min(1),
-  DOMAIN: z.string().min(1),
-  DATABASE_URL: z.string().min(1),
-  SPOTIFY_CLIENT_ID: z.string().min(1),
-  SPOTIFY_CLIENT_SECRET: z.string().min(1),
-  COOKIE_SECRET: z.string().min(1),
+  PORT: nonEmptyString,
+  DOMAIN: nonEmptyString,
+  DATABASE_URL: nonEmptyString,
+  SPOTIFY_CLIENT_ID: nonEmptyString,
+  SPOTIFY_CLIENT_SECRET: nonEmptyString,
 });
 
 // Validate the environment variables against the schema
